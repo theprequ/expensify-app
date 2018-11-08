18f8556391c1b8ee78e21f087c685bb7dbf1adce
Mark <ptunnus@gmail.com>
2018-11-08 15:38:18 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,52 @@
 const path = require("path"); // Load path module (https://nodejs.org/api/path.html#path_path_join_paths)
+const ExtractTextPlugin = require("extract-text-webpack-plugin") // S13 L134
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+// Was changed from exporting an object to returning a function
+// ^^ In S13 L133
+module.exports = (env) => {
+    const isProduction = env === "production";
+    const CSSExtract = new ExtractTextPlugin("styles.css");
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
+
+// THIS FILE WAS LAST MODIFIED IN SECTION 13 LECTURE 134
