a6f26642d8d768a7e08490a1cf32f36b14eeb13e
Mark <ptunnus@gmail.com>
2018-11-13 11:44:48 +0200

    Add private routes

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import expensesReducer from "../reducers/expenses";
 import filtersReducer from "../reducers/filters";
-import thunk from "redux-thunk";
+import authReducer from "../reducers/auth";
 
 // If we're using Redux devtools, we're going to use it. If not, we just use compose:
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -13,7 +14,8 @@ export default () => {
     const store = createStore(
         combineReducers({
             expenses: expensesReducer,
-            filters: filtersReducer
+            filters: filtersReducer,
+            auth: authReducer
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
